f047c9f0debe0398912798293c3d1346deff5cd2
charvi-web <charvisingh077@gmail.com>
2026-01-25 11:29:06 +0530

    variables and datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553  //can't be changed
+//variables ko delare krna
+let accountEmail = "charvi@google.com"
+var accountPassword = "charvi123" //scope ki wajah se avoid krna chahiye
+accountCity = "New Delhi"
+let acccountSate  //undefined
+
+// accountId = 123456 // not allowed
+accountEmail = "cs@charvi.com"
+accountPassword = "nishi@123"
+accountCity = "Noida"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,acccountSate]);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as new version
+// alert(3+3);  we r using nodejs not browser
+
+console.log(9+7)  
+
+let name = "Charvi"
+let age = 20
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => '',"",``
+//boolean => true/false
+//null => standalone value khali h ,temp aaya nhi toh 0 degree nhi dikha skte.
+//undefined => value is not assigned
+//symbol => unique
+
+//object
+console.log(typeof name)  //string
+console.log(typeof null)  //object
+console.log(typeof undefined)  //undefined
